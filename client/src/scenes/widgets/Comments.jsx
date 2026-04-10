af681fc200985ed39c7c74eeb94f0d8abe804795
ak5113165 <ak5113165@gmail.com>
2026-04-10 19:57:20 +0530

    first commit

---
+++ client/src/scenes/widgets/Comments.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyPostWidget from "./MyPostWidget";
+
+const Comments = () => {
+  return (
+    <div>
+      <MyPostWidget/>
+    </div>
+  );
+};
+
+export default Comments;
